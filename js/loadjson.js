2d34c1fb97fb6ec4d1ee7a34d7de31041d05b4f6
Stan <stanislav.siska@yahoo.com>
2017-03-14 20:58:43 +0100

    Fixed page history Renamed top menu names Updated get JSON schema example

--- js/loadjson.js
+++ js/loadjson.js
@@ -1,53 +1,39 @@
 // Documentation available @ http://api.jquery.com/jquery.getjson/
 $(document).ready(function () {
     // $('#show-data').click(function () {
-
-        var showData1 = $('#show-player-1');
+        var html = '';
+        var showData1 = $('#show-player-data');
         $.getJSON('example.json', function (data) {
 
+            console.log(data.body.Item.user);
             // Might be necessary to create tables containing data, simple list is not enough
             // Display card pictures
-            var player1 = data.player1.map(function (item1) {
-                console.log(item1);
-                return 'Player1: ' + item1.name + '  ' +
-                    '<img src="img/' + item1.card1 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item1.card2 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item1.card3 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item1.card4 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item1.card5 + '.png" height="40px" width="40px" align="center">'
-            });
+
+            html += '<h5>User name:' + data.body.Item.user +' </h5>';
+            html += '<div class="col col-md-6"> Wins : ' + data.body.Item.wins +' </div>';
+            html += '<div class="col col-md-6"> Draw : ' + data.body.Item.draw +' </div>';
+            html += '<div class="col col-md-6"> Lose : ' + data.body.Item.lose +' </div>';
+            // html += '<div class="col col-md-6"> History : ' + data.body.Item.history.values +' </div>';
+
+            // var player = data.player.map(function (record) {
+            //     console.log(record);
+            //
+            //     return 'Player: ' + record.name + '  ' +
+            //         '<img src="img/' + record.card1 + '.png" height="40px" width="40px" align="center">' +
+            //         '<img src="img/' + record.card2 + '.png" height="40px" width="40px" align="center">' +
+            //         '<img src="img/' + record.card3 + '.png" height="40px" width="40px" align="center">' +
+            //         '<img src="img/' + record.card4 + '.png" height="40px" width="40px" align="center">' +
+            //         '<img src="img/' + record.card5 + '.png" height="40px" width="40px" align="center">'
+            // });
+
             showData1.empty();
 
-            var content = '<li href="#" class="list-group-item">' + player1.join('</li><li href="#" class="list-group-item">') + '</li>';
-            var list = $('<ul class="list-group">').html(content);
+            // var content = '<li href="#" class="list-group-item">' + player.join('</li><li href="#" class="list-group-item">') + '</li>';
+            var list = $('<table class="table">').html(html);
 
             showData1.append(list);
         });
 
         showData1.text('Loading the JSON file.');
 
-        // -----
-
-        var showData2 = $('#show-player-2');
-        $.getJSON('example.json', function (data) {
-
-            // Display card pictures
-            var player2 = data.player2.map(function (item2) {
-                console.log(item2);
-                return 'Player2: ' + item2.name + '<br>' +
-                    '<img src="img/' + item2.card1 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item2.card2 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item2.card3 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item2.card4 + '.png" height="40px" width="40px" align="center">' +
-                    '<img src="img/' + item2.card5 + '.png" height="40px" width="40px" align="center">'
-            });
-            showData2.empty();
-
-            var content = '<li href="#" class="list-group-item">' + player2.join('</li><li href="#" class="list-group-item">') + '</li>';
-            var list = $('<ul class="list-group">').html(content);
-
-            showData2.append(list);
-        });
-
-        showData2.text('Loading the JSON file.');
 });
