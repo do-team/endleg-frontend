4cdf5f71ae11ef59aa47454d55cfa95bbff9b323
Stan <stanislav.siska@yahoo.com>
2017-03-14 21:24:58 +0100

    bugfixes

--- js/auth.js
+++ js/auth.js
@@ -70,7 +70,7 @@ function confirmUser(username, code) {
         }
         alert('User ' + username + '  Confirmed');
     });
-    window.location = "./profile.html";
+    window.location = "./index.html";
 }
 
 function loginUser(username, password) {
@@ -262,3 +262,43 @@ function getName() {
     }
     document.getElementById("myCard1").value = filename;
 }
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId   : '927138690721496',
+        oauth   : true,
+        status  : true, // check login status
+        cookie  : true, // enable cookies to allow the server to access the session
+        xfbml   : true // parse XFBML
+    });
+};
+
+function fb_login(){
+    FB.login(function(response) {
+
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            //console.log(response); // dump complete info
+            access_token = response.authResponse.accessToken; //get access token
+            user_id = response.authResponse.userID; //get FB UID
+
+            FB.api('/me', function(response) {
+                user_email = response.email; //get user email
+                // you can store this data into your database
+            });
+
+        } else {
+            //user hit cancel button
+            console.log('User cancelled login or did not fully authorize.');
+
+        }
+    }, {
+        scope: 'publish_stream,email'
+    });
+}
+(function() {
+    var e = document.createElement('script');
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+    e.async = true;
+    document.getElementById('fb-root').appendChild(e);
+}());
--- js/loadjson.js
+++ js/loadjson.js
@@ -3,37 +3,50 @@ $(document).ready(function () {
     // $('#show-data').click(function () {
         var html = '';
         var showData1 = $('#show-player-data');
+
         $.getJSON('example.json', function (data) {
 
-            console.log(data.body.Item.user);
-            // Might be necessary to create tables containing data, simple list is not enough
-            // Display card pictures
+        // $.getJSON('https://y3op33lkfd.execute-api.eu-central-1.amazonaws.com/PROD/out', function (data) {
 
-            html += '<h5>User name:' + data.body.Item.user +' </h5>';
-            html += '<div class="col col-md-6"> Wins : ' + data.body.Item.wins +' </div>';
-            html += '<div class="col col-md-6"> Draw : ' + data.body.Item.draw +' </div>';
-            html += '<div class="col col-md-6"> Lose : ' + data.body.Item.lose +' </div>';
-            // html += '<div class="col col-md-6"> History : ' + data.body.Item.history.values +' </div>';
+                console.log(data.body.Item.user);
+                // Might be necessary to create tables containing data, simple list is not enough
+                // Display card pictures
 
-            // var player = data.player.map(function (record) {
-            //     console.log(record);
-            //
-            //     return 'Player: ' + record.name + '  ' +
-            //         '<img src="img/' + record.card1 + '.png" height="40px" width="40px" align="center">' +
-            //         '<img src="img/' + record.card2 + '.png" height="40px" width="40px" align="center">' +
-            //         '<img src="img/' + record.card3 + '.png" height="40px" width="40px" align="center">' +
-            //         '<img src="img/' + record.card4 + '.png" height="40px" width="40px" align="center">' +
-            //         '<img src="img/' + record.card5 + '.png" height="40px" width="40px" align="center">'
-            // });
+                html += '<h5>Username: ' + data.body.Item.user +' </h5>';
 
-            showData1.empty();
+                html += '<div class="row">';
+                html += '<div class="col col-md-4"> Wins : ' + data.body.Item.wins +' </div>';
+                html += '<div class="col col-md-4"> Draw : ' + data.body.Item.draw +' </div>';
+                html += '<div class="col col-md-4"> Lose : ' + data.body.Item.lose +' </div>';
+                html += '</div> </br>';
 
-            // var content = '<li href="#" class="list-group-item">' + player.join('</li><li href="#" class="list-group-item">') + '</li>';
-            var list = $('<table class="table">').html(html);
+                html += '<div class="row">';
+                html += '<div class="col col-md-12"> <h5>History records: </h5> <br> ' + data.body.Item.history.values +' </div>';
 
-            showData1.append(list);
+                html += '</div>';
+
+                // html += '<div class="col col-md-6"> History : ' + data.body.Item.history.values +' </div>';
+
+                // var player = data.player.map(function (record) {
+                //     console.log(record);
+                //
+                //     return 'Player: ' + record.name + '  ' +
+                //         '<img src="img/' + record.card1 + '.png" height="40px" width="40px" align="center">' +
+                //         '<img src="img/' + record.card2 + '.png" height="40px" width="40px" align="center">' +
+                //         '<img src="img/' + record.card3 + '.png" height="40px" width="40px" align="center">' +
+                //         '<img src="img/' + record.card4 + '.png" height="40px" width="40px" align="center">' +
+                //         '<img src="img/' + record.card5 + '.png" height="40px" width="40px" align="center">'
+                // });
+
+                showData1.empty();
+
+                // var content = '<li href="#" class="list-group-item">' + player.join('</li><li href="#" class="list-group-item">') + '</li>';
+                var list = $('<table class="table">').html(html);
+
+                showData1.append(list);
+            });
         });
 
-        showData1.text('Loading the JSON file.');
+        // showData1.text('Loading the JSON file.')
 
-});
+// })
